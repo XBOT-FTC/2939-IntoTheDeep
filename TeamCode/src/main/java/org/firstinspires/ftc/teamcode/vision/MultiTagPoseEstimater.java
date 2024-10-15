e2fb3ebb5b6d6a8475b1649795afa5486d15336b
kujo27 <johnkuang783@gmail.com>
2024-10-14 20:25:12 -0700

    installed opencv

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/MultiTagPoseEstimater.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.vision;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagPoseFtc;
+import org.opencv.calib3d.Calib3d;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.MatOfPoint3f;
+import org.opencv.core.Point;
+import org.opencv.core.Point3;
+
+import java.util.List;
+
+public class MultiTagPoseEstimater {
+
+}
