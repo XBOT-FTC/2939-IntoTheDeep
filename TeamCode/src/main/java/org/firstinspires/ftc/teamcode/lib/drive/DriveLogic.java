e462b29c0e824230c506ecaaee47c6651163bca6
kujo27 <johnkuang783@gmail.com>
2024-10-23 17:12:45 -0700

    drive constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/drive/DriveLogic.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/drive/DriveLogic.java
@@ -30,18 +30,59 @@ public class DriveLogic {
         double goalHeading;
 
         //TODO: refine quadrants
-        if (rightJoystickY > 0.7 && Math.abs(rightJoystickX) < 0.1) {
+        if (rightJoystickY > 0.7 && Math.abs(rightJoystickX) < 0.2) {
             goalHeading = 0;
-        } else if (rightJoystickX < -0.7 && Math.abs(rightJoystickY) < 0.1) {
+        } else if (rightJoystickX < -0.7 && Math.abs(rightJoystickY) < 0.2) {
             goalHeading = 90;
-        } else if (rightJoystickY < -0.7 && Math.abs(rightJoystickX) < 0.1) {
+        } else if (rightJoystickY < -0.7 && Math.abs(rightJoystickX) < 0.2) {
             goalHeading = 180;
-        } else if (rightJoystickX > 0.7 && Math.abs(rightJoystickY) < 0.1) {
+        } else if (rightJoystickX > 0.7 && Math.abs(rightJoystickY) < 0.2) {
             goalHeading = -90;
         } else {
             goalHeading = currentHeading;
         }
 
+        // Calculate the error between the desired heading and the current heading
+        double error = goalHeading - currentHeading;
+
+        // Ensure the error is within the range [-180, 180]
+        if (error > 180) {
+            error -= 360;
+        } else if (error < -180) {
+            error += 360;
+        }
+        double absError = Math.abs(error);
+
+        // doing this so the state is always less than the target
+        //TODO: if this doesn't work add a big number (180, 360, 1000) to both variables so the error that the pid calculates is always positive
+        double state = Math.abs(goalHeading) - absError;
+        double target = Math.abs(goalHeading);
+
+        double headingPower = pidManager.pidControl(state, target);
+
+        // handling which direction to rotate
+        if (error > 0) {
+            headingPower *= -1;
+        }
+
+        telemetry.addData("currentHeading", currentHeading);
+        telemetry.addData("goalHeading", goalHeading);
+        telemetry.addData("state", state);
+        telemetry.addData("target", target);
+        telemetry.addData("error", error);
+        telemetry.addData("absError", absError);
+        telemetry.addData("headingPower", headingPower);
+        if (error > 0 ) {
+            telemetry.addLine("Supposed to be turning LEFT");
+        }else {
+            telemetry.addLine("Supposed to be turning RIGHT");
+        }
+
+        return headingPower;
+    }
+
+    public static double getQuickRotateToHeadingPower(double currentHeading, double goalHeading,
+                                                      PIDManager pidManager, Telemetry telemetry) {
         // Calculate the error between the desired heading and the current heading
         double error = goalHeading - currentHeading;
 
