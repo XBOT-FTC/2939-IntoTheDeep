aaf2cd1ed5e2ed859dc34708a7dc463b9e94505a
anthonytrenh <trenhanthony@gmail.com>
2024-10-26 12:27:35 -0700

    set up arm in teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.teamcode.ElectricalContract;
+import org.firstinspires.ftc.teamcode.lib.arm.Arm;
 import org.firstinspires.ftc.teamcode.lib.drive.JohnMecanumDrive;
 import org.firstinspires.ftc.teamcode.lib.intake.Intake;
 
@@ -19,6 +20,7 @@ public class Teleop extends LinearOpMode{
         IMU imu = hardwareMap.get(IMU.class, ElectricalContract.imu());
         JohnMecanumDrive drive = new JohnMecanumDrive(hardwareMap);
         Intake intake = new Intake(hardwareMap);
+        Arm arm = new Arm(hardwareMap);
 
         //TODO:
         // IMU adjustments for RevHub orientation
@@ -33,6 +35,7 @@ public class Teleop extends LinearOpMode{
         while (opModeIsActive()) {
             drive.drive(gamepad1, imu, telemetry);
             intake.controls(gamepad2, telemetry);
+            arm.controls(gamepad2, telemetry);
 
             telemetry.update();
         }
