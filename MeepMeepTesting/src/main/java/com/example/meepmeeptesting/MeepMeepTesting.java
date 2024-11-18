c6771cbc084bd93fa4b5c5112eeb8126815ff26e
kujo27 <johnkuang783@gmail.com>
2024-11-17 18:29:53 -0800

    fixes

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -2,6 +2,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Rotation2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
@@ -16,26 +17,17 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        //starting pose 6, -62
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(6, -62, Math.toRadians(270)))
+        //starting pose 6, -62 for specimen
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-37, -62, Math.toRadians(270)))
                 .setReversed(true)
                 .waitSeconds(2)
-                .lineToY(-33)
-//                .waitSeconds(2)
+                .splineToLinearHeading(new Pose2d(-48, -50, Math.toRadians(-135)), Math.toRadians(270))
+                .waitSeconds(2)
+                .turnTo(Math.toRadians(270))
+                .lineToY(-43)
+                .waitSeconds(2)
+                .splineToLinearHeading(new Pose2d(-48, -50, Math.toRadians(-135)), Math.toRadians(270))
 
-
-//                .splineToLinearHeading(new Pose2d(38, -10, Math.toRadians(180)), Math.toRadians(90))
-//                .strafeToConstantHeading(new Vector2d(45, -10))
-//                .setReversed(false)
-//                .strafeToConstantHeading(new Vector2d(45, -55))
-//                .strafeToConstantHeading(new Vector2d(45, -10))
-//                .strafeToConstantHeading(new Vector2d(55, -10))
-//                .strafeToConstantHeading(new Vector2d(55, -55))
-//                .strafeToConstantHeading(new Vector2d(55, -10))
-//                .strafeToConstantHeading(new Vector2d(61, -10))
-//                .strafeToConstantHeading(new Vector2d(61, -55))
-//                .setTangent(90)
-//                .splineToLinearHeading(new Pose2d(29, -11, Math.toRadians(-90)), Math.toRadians(180))
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/RoadRunnerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/RoadRunnerTest.java
@@ -10,116 +10,116 @@ import com.acmerobotics.roadrunner.InstantAction;
 import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
-import com.acmerobotics.roadrunner.SleepAction;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.teamcode.lib.Constants;
 import org.firstinspires.ftc.teamcode.lib.arm.ArmClaw;
 import org.firstinspires.ftc.teamcode.lib.arm.ArmRotation;
 import org.firstinspires.ftc.teamcode.lib.arm.ArmSlide;
 import org.firstinspires.ftc.teamcode.lib.arm.ArmWrist;
 import org.firstinspires.ftc.teamcode.lib.intake.IntakePivot;
+import org.firstinspires.ftc.teamcode.lib.intake.IntakeWheels;
 import org.firstinspires.ftc.teamcode.rr.MecanumDrive;
 
 
 @Config
 @Autonomous(name= "RoadRunnerTest", group="Autonomous")
 public class RoadRunnerTest extends LinearOpMode {
-    ArmSlide slide = new ArmSlide(hardwareMap);
-    ArmRotation rotation = new ArmRotation(hardwareMap);
-    ArmClaw grabber =  new ArmClaw(hardwareMap);
-    ArmWrist wrist = new ArmWrist(hardwareMap);
-    IntakePivot pivot = new IntakePivot(hardwareMap);
-    ArmSlide.SlidePositions slidePosition = ArmSlide.SlidePositions.HOMED;
-
-//    public Action slideToHighBasket(Telemetry telemetry) {
-//        return new Action() {
-//            private boolean initialized = false;
-//
-//            @Override
-//            public boolean run(@NonNull TelemetryPacket packet) {
-//                if (!initialized) {
-//                    slide.slide(telemetry, ArmSlide.SlidePositions.HIGH_BASKET);
-//                    initialized = true;
-//                }
-//
-//                double pos = slide.getCurrentPosition();
-//                packet.put("slidePosition", pos);
-//                return pos < Constants.highBasketSlideExtension;
-//            }
-//        };
-//    }
-
-//    public Action armSlideUpdate(ArmSlide.SlidePositions slidePositions, Telemetry telemetry) {
-//        return new Action() {
-//            private boolean initialized = false;
-//            @Override
-//            public boolean run(@NonNull TelemetryPacket packet) {
-//                if (!initialized) {
-//                    slide.slide(telemetry, slidePositions);
-//                    initialized = true;
-//                }
-//
-//                double pos = slide.getCurrentPosition();
-//                packet.put("slidePosition", pos);
-//                return false;
-//            }
-//        };
-//    }
-
-//    public Action initSystems() {
-//        return new SequentialAction(
-//                new InstantAction(() -> {
-//                    pivot.home();
-//                }),
-//                new InstantAction(() -> {
-//                    slidePosition = ArmSlide.SlidePositions.LIFT_HIGH_SPECIMEN;
-//                }),
-//                new InstantAction(() -> {
-//                    wrist.scoreHighSpecimen();
-//                }),
-//                new InstantAction(() -> {
-//                    rotation.specimenHighPosition();
-//                })
-//        );
-//    }
+    ArmSlide slide;
+    ArmRotation rotation;
+    ArmClaw grabber;
+    ArmWrist wrist;
+    IntakePivot pivot;
+    IntakeWheels intake;
+    ArmSlide.SlidePositions slidePosition;
 
     @Override
     public void runOpMode() {
-        Pose2d startingPose = new Pose2d(6, -62, Math.toRadians(270));
+        slide = new ArmSlide(hardwareMap);
+        rotation = new ArmRotation(hardwareMap);
+        grabber =  new ArmClaw(hardwareMap);
+        wrist = new ArmWrist(hardwareMap);
+        pivot = new IntakePivot(hardwareMap);
+        slidePosition = ArmSlide.SlidePositions.HOMED;
+
+        Pose2d startingPose = new Pose2d(-37, -62, Math.toRadians(270));
 
         MecanumDrive drive = new MecanumDrive(hardwareMap, startingPose);
 
         Action trajectoryAction = drive.actionBuilder(drive.pose)
                 .setReversed(true)
-//                .afterTime(0, new SequentialAction(
-//                        new InstantAction(() -> {
-//                            slidePosition = ArmSlide.SlidePositions.HIGH_SPECIMEN;
-//                        })))
+                .splineToLinearHeading(new Pose2d(-48, -50, Math.toRadians(-135)), Math.toRadians(270))
                 .waitSeconds(2)
-                .lineToY(-33)
+                .afterTime(2, new SequentialAction(
+                        new InstantAction(() -> {
+                            slidePosition = ArmSlide.SlidePositions.HIGH_BASKET;
+                        }),
+                        new InstantAction(() -> {
+                            rotation.basketPosition();
+                        })))
+                .afterTime(3, new SequentialAction(
+                        new InstantAction(() -> {
+                            grabber.open();
+                        })))
+//                .turnTo(Math.toRadians(270))
+//                .waitSeconds(1)
+//                .lineToY(-43)
+//                .waitSeconds(2)
+//                .splineToLinearHeading(new Pose2d(-48, -50, Math.toRadians(-135)), Math.toRadians(270))
                 .build();
 
 
-//        while (!isStopRequested() && !opModeIsActive()) {
-//            telemetry.addLine("Some telemetry here.");
-//            telemetry.update();
-//        }
+        while (!isStopRequested() && !opModeIsActive()) {
+            telemetry.addLine("Some telemetry here.");
+            telemetry.update();
+        }
 
         Actions.runBlocking(
                 new SequentialAction(
-//                        initSystems(),
-//                        new ParallelAction(
-//                                trajectoryAction,
-//                                armSlideUpdate(slidePosition, telemetry)
-//                        )
-                        trajectoryAction
+                        initSystems(),
+                        new ParallelAction(
+                                trajectoryAction,
+                                armSlideUpdate(slidePosition, telemetry)
+                        )
                 )
 
         );
 
+    }
+
+    public Action armSlideUpdate(ArmSlide.SlidePositions slidePositions, Telemetry telemetry) {
+        return new Action() {
+            private boolean initialized = false;
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    slide.slide(telemetry, slidePositions);
+                    initialized = true;
+                }
+
+                double pos = slide.getCurrentPosition();
+                packet.put("slidePosition", pos);
+                return false;
+            }
+        };
+    }
+
+    public Action initSystems() {
+        return new SequentialAction(
+                new InstantAction(() -> {
+                    slidePosition = ArmSlide.SlidePositions.TRANSFER;
+                }),
+                new InstantAction(() -> {
+                    grabber.close();
+                }),
+                new InstantAction(() -> {
+                    wrist.score();
+                }),
+                new InstantAction(() -> {
+                    pivot.home();
+                })
+        );
     }
 }
